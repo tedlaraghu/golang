12b197665975c6e7ceee84486e9fb59fcb11e0b0
Raghu <rtedia@woolworths.com.au>
2026-03-16 21:08:34 +1100

    c3

--- edgarfacts/cmd/facts/facts.go
+++ edgarfacts/cmd/facts/facts.go
@@ -1,9 +1,9 @@
-package facts
+package main
 
 import (
 	"fmt"
-	"internal/facts"
-	// "github.com/tedlaraghu/edgarfacts/internal/facts"
+	// "internal/facts"
+	"github.com/tedlaraghu/edgarfacts/internal/facts"
 )
 
 func main() {
@@ -12,11 +12,11 @@ func main() {
 	name := "RT"
 	email := "rt@example.com"
 
-	facts, err := facts.LoadFacts(cik, name, organization, email)
+	factData, err := facts.LoadFacts(cik, name, organization, email)
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(facts))
+	fmt.Println(string(factData))
 }
