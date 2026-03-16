973f987324f9343d2c7d47e536a53f4ea1fb965d
Raghu <rtedia@woolworths.com.au>
2026-03-16 21:11:52 +1100

    c4

--- edgarfacts/cmd/facts/facts.go
+++ edgarfacts/cmd/facts/facts.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	// "internal/facts"
-	"github.com/tedlaraghu/edgarfacts/internal/facts"
+	"github.com/tedlaraghu/edgarfacts/internal/facts/facts.go"
 )
 
 func main() {
