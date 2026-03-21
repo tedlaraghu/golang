81d1ff393d9f50a6c189cc7902803ab774eda21e
tedlaraghu <54474395+tedlaraghu@users.noreply.github.com>
2026-03-22 08:42:45 +1100

    Update facts.go

--- edgarfacts/cmd/facts/facts.go
+++ edgarfacts/cmd/facts/facts.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	// "internal/facts"
-	"github.com/tedlaraghu/edgarfacts/internal/facts"
+	"github.com/tedlaraghu/golnag/edgarfacts/internal/facts"
 )
 
 func main() {
