074b7843857d8b710a9435399668565a0234e36e
Raghu <rtedia@woolworths.com.au>
2026-03-22 21:26:01 +1100

    m12

--- edgarfacts/cmd/facts/facts.go
+++ edgarfacts/cmd/facts/facts.go
@@ -81,30 +81,3 @@ func main() {
 	}
 
 }
-
-// func main() {
-// 	cik := "0000886982"
-// 	organization := "My Organization"
-// 	name := "RT"
-// 	email := "rt@example.com"
-
-// 	factData, err := facts.LoadFacts(cik, name, organization, email)
-
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	// fmt.Println(string(factData))
-
-// 	bucketName := "gostoragert"
-// 	filePath := fmt.Sprintf("sec/edgar/facts/stage/%s.json", cik)
-
-// 	err = storage.UploadBytes(factData, bucketName, filePath)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	//Log Upload
-// 	fmt.Printf("Uploaded %s\n", cik)
-
-// }
