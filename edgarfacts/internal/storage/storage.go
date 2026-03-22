4eb7ccb0079c8e5256badd58c6e9b8295dd7057a
Raghu <rtedia@woolworths.com.au>
2026-03-22 18:05:37 +1100

    m5

---
+++ edgarfacts/internal/storage/storage.go
@@ -0,0 +1,47 @@
+package storage
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"time"
+
+	"cloud.google.com/go/storage"
+)
+
+// upload Bytes
+func uploadBytes(data []byte, bucket, path string) error {
+
+	//Create buffer
+	buffer := bytes.NewBuffer(data)
+
+	//Create client
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	defer client.Close()
+
+	//create writer
+	ctx, cancel := context.WithTimeout(ctx, 120*time.Second)
+	defer cancel()
+
+	writer := client.Bucket(bucket).Object(path).NewWriter(ctx)
+
+	//copy data from buffer to Google cloud storage
+	_, err = io.Copy(writer, buffer)
+	if err != nil {
+		return err
+	}
+
+	//close writer
+	err = writer.Close()
+	if err != nil {
+		return nil
+	}
+
+	// Return Result
+	return nil
+}
